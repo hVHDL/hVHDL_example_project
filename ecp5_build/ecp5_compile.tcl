8e41765d50c62a449627344d2d2678f501d278e6
jari <jari.honkanen@danfoss.com>
2022-07-13 23:44:21 +0300

    add bitgen task to compile in order to get bitfile

--- ecp5_build/ecp5_compile.tcl
+++ ecp5_build/ecp5_compile.tcl
@@ -63,5 +63,5 @@ prj_run Synthesis -impl impl1
 prj_run Translate -impl impl1
 prj_run Map -impl impl1
 prj_run PAR -impl impl1
-prj_run Export -impl impl1
+prj_run Export -impl impl1 -task Bitgen
 prj_project save
