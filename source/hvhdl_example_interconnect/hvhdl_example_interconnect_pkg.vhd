3494101005d4a76ece20cc6dd72085a2e80aaf7e
jari <jari.honkanen@danfoss.com>
2022-07-14 09:49:48 +0300

    update signal generation

--- source/hvhdl_example_interconnect/hvhdl_example_interconnect_pkg.vhd
+++ source/hvhdl_example_interconnect/hvhdl_example_interconnect_pkg.vhd
@@ -40,8 +40,9 @@ architecture rtl of hvhdl_example_interconnect is
 
     
     signal multiplier : multiplier_record := init_multiplier;
+    signal multiplier2 : multiplier_record := init_multiplier;
     signal sincos : sincos_record := init_sincos;
-    signal angle : integer  range 0 to 2**16-1;
+    signal angle : integer  range 0 to 2**16-1 := 0;
     signal i : integer range 0 to 2**16-1 := 1199;
 
     signal communications_clocks   : communications_clock_group;
@@ -54,6 +55,8 @@ architecture rtl of hvhdl_example_interconnect is
 
     signal filter : first_order_filter_record := init_first_order_filter;
     signal prbs7 : std_logic_vector(6 downto 0) := (0 => '1', others => '0');
+    signal harmonic_counter : integer range 0 to 15 := 15;
+    signal sine_with_noise : int := 0;
 
 begin
 
@@ -62,16 +65,16 @@ begin
     begin
         if rising_edge(system_clock) then
             create_multiplier(multiplier);
+            create_multiplier(multiplier2);
             create_sincos(multiplier, sincos);
-            create_first_order_filter(filter =>filter,multiplier => multiplier, time_constant => 0.001);
-
+            create_first_order_filter(filter =>filter,multiplier => multiplier2, time_constant => 0.001);
 
             init_bus(bus_out);
-            connect_read_only_data_to_address(bus_in, bus_out, 100, get_sine(sincos));
+            connect_read_only_data_to_address(bus_in, bus_out, 100, get_sine(sincos)/2 + 32768);
             connect_read_only_data_to_address(bus_in, bus_out, 101, angle);
-            connect_read_only_data_to_address(bus_in, bus_out, 102, to_integer(unsigned(prbs7)));
-            connect_read_only_data_to_address(bus_in, bus_out, 103, get_sine(sincos) + to_integer(signed(prbs7)));
-            connect_read_only_data_to_address(bus_in, bus_out, 104, get_filter_output(filter));
+            connect_read_only_data_to_address(bus_in, bus_out, 102, to_integer(signed(prbs7))+32768);
+            connect_read_only_data_to_address(bus_in, bus_out, 103, sine_with_noise/2 + 32768);
+            connect_read_only_data_to_address(bus_in, bus_out, 104, get_filter_output(filter)/2 + 32678);
 
 			if i > 0 then
 				i <= (i - 1);
@@ -81,14 +84,15 @@ begin
 
             if i = 0 then
                 request_sincos(sincos, angle);
+                harmonic_counter <= 0;
             end if;
-
             
             if sincos_is_ready(sincos) then
-                angle <= (angle + 55) mod 2**16;
-                prbs7 <= prbs7(5 downto 0) & prbs7(6);
+                angle    <= (angle + 10) mod 2**16;
+                prbs7    <= prbs7(5 downto 0) & prbs7(6);
                 prbs7(6) <= prbs7(5) xor prbs7(0);
-                filter_data(filter, get_sine(sincos) + to_integer(signed(prbs7)));
+                filter_data(filter, sine_with_noise);
+                sine_with_noise <= get_sine(sincos) + to_integer(signed(prbs7)*64);
             end if;
 
         end if; --rising_edge
