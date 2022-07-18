fab66c60d3997e87d2e65794e61688d46a3bfb99
jari <jaarhonka@yahoo.com>
2022-07-18 09:50:14 +0300

    fix names in fixed point filter

--- source/hvhdl_example_interconnect/arch_fixed_example_filter_entity.vhd
+++ source/hvhdl_example_interconnect/arch_fixed_example_filter_entity.vhd
@@ -13,19 +13,19 @@ architecture fixed_point of example_filter_entity is
     use work.first_order_filter_pkg.all;
     use work.multiplier_pkg.all;
     signal filter : first_order_filter_record := init_first_order_filter;
-    signal filtered_sine : int := 0;
+    signal scaled_sine : int := 0;
 
     signal multiplier2 : multiplier_record := init_multiplier;
     signal process_counter : integer range 0 to 3 := 3;
 
 begin
 
-    floating_point_filter : process(clock)
+    fixed_point_filter : process(clock)
     begin
         if rising_edge(clock) then
             init_bus(bus_out);
             connect_read_only_data_to_address(bus_in, bus_out, 104, get_filter_output(filter)/2 + 32678);
-            connect_read_only_data_to_address(bus_in, bus_out, 105, filtered_sine/2 + 32678);
+            connect_read_only_data_to_address(bus_in, bus_out, 105, scaled_sine/2 + 32678);
             create_multiplier(multiplier2);
             create_first_order_filter(filter => filter , multiplier => multiplier2 , time_constant => filter_time_constant);
 
@@ -42,14 +42,13 @@ begin
                     end if;
                 WHEN 1 => 
                     if multiplier_is_ready(multiplier2) then
-                        filtered_sine <= get_multiplier_result(multiplier2, 15);
+                        scaled_sine <= get_multiplier_result(multiplier2, 15);
                         process_counter <= process_counter + 1;
                     end if;
                 WHEN others => -- wait for start
             end CASE;
 
-
         end if; --rising_edge
-    end process floating_point_filter;	
+    end process;	
 
 end fixed_point;
