2d70f55fe98a6b193e250450fcd65c43d6d87dd7
jari <jari.honkanen@danfoss.com>
2022-09-02 17:20:23 +0300

    add filter with procedure call

--- source/hvhdl_example_interconnect/arch_float_example_filter_entity.vhd
+++ source/hvhdl_example_interconnect/arch_float_example_filter_entity.vhd
@@ -45,6 +45,31 @@ begin
         ------------------------------------------------------------------------
             create_first_order_filter(float_filter, float_alu, to_float(filter_time_constant));
         ------------------------------------------------------------------------
+            -- filter_is_ready <= false;
+            -- CASE filter_counter is
+            --     WHEN 0 => 
+            --         subtract(float_alu, u, y);
+            --         filter_counter <= filter_counter + 1;
+            --     WHEN 1 =>
+            --         if add_is_ready(float_alu) then
+            --             multiply(float_alu  , get_add_result(float_alu) , filter_gain);
+            --             filter_counter <= filter_counter + 1;
+            --         end if;
+            --
+            --     WHEN 2 =>
+            --         if multiplier_is_ready(float_alu) then
+            --             add(float_alu, get_multiplier_result(float_alu), y);
+            --             filter_counter <= filter_counter + 1;
+            --         end if;
+            --     WHEN 3 => 
+            --         if add_is_ready(float_alu) then
+            --             y <= get_add_result(float_alu);
+            --             filter_counter <= filter_counter + 1;
+            --             filter_is_ready <= true;
+            --         end if;
+            --     WHEN others =>  -- wait for start
+            -- end CASE;
+        ------------------------------------------------------------------------
 
             if example_filter_input.filter_is_requested then
                 convert_integer_to_float(float_to_integer_converter, example_filter_input.filter_input, 15);
