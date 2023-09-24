9f0d52126eab8c41a081096e7deff561d88e64b2
Jari Honkanen <jaarhonka@yahoo.com>
2023-09-24 20:41:48 +0300

    move ram out of processor record

--- source/main/arch_microprogram_processor.vhd
+++ source/main/arch_microprogram_processor.vhd
@@ -37,10 +37,17 @@ architecture microprogram of example_filter_entity is
     constant low_pass_filter : program_array := get_pipelined_low_pass_filter;
     constant test_program    : program_array := get_dummy & get_pipelined_low_pass_filter;
 
-    signal ram_contents : ram_array := init_ram(test_program);
-    signal self         : processor_with_ram_record := init_processor(test_program'high);
+    signal self                      : processor_with_ram_record := init_processor(test_program'high);
+    signal ram_read_instruction_port : ram_read_port_record    := init_ram_read_port ;
+    signal ram_read_data_port        : ram_read_port_record    := init_ram_read_port ;
+    signal ram_write_port            : ram_write_port_record   := init_ram_write_port;
+    signal ram_write_port2           : ram_write_port_record   := init_ram_write_port;
 
     signal result : integer range -2**15 to 2**15-1 := 0;
+
+    signal ram_contents : ram_array := write_register_values_to_ram(
+            init_ram(test_program), 
+            to_fixed((0.0 , 0.44252 , 0.1   , 0.1   , 0.1   , 0.1   , 0.1   , 0.0104166 , 0.1)   , 19) , 53-reg_array'length*2);
 
 begin
 
@@ -49,41 +56,49 @@ begin
         begin
             self.program_counter <= dummy'length;
         end request_low_pass_filter;
+
+
+
+        constant register_memory_start_address : integer := ram_contents'length-self.registers'length;
     begin
         if rising_edge(clock) then
             init_bus(bus_out);
             connect_read_only_data_to_address(bus_in, bus_out, 15165 , result/2 + 32768);
 
-            create_ram_read_port(self.ram_read_instruction_port);
-            create_ram_read_port(self.ram_read_data_port);
-            create_ram_write_port(self.ram_write_port);
-            create_ram_write_port(self.ram_write_port2);
-            create_processor_w_ram(self, ram_contents'length);
-
+            create_ram_read_port(ram_read_instruction_port);
+            create_ram_read_port(ram_read_data_port);
+            create_ram_write_port(ram_write_port);
+            -- create_ram_write_port(ram_write_port2);
             --------------------
-            if read_is_requested(self.ram_read_instruction_port) then
-                self.ram_read_instruction_port.data <= ram_contents(get_ram_address(self.ram_read_instruction_port));
+            if read_is_requested(ram_read_instruction_port) then
+                ram_read_instruction_port.data <= ram_contents(get_ram_address(ram_read_instruction_port));
             end if;
             --------------------
-            if read_is_requested(self.ram_read_data_port) then
-                self.ram_read_data_port.data <= ram_contents(get_ram_address(self.ram_read_data_port));
+            if read_is_requested(ram_read_data_port) then
+                ram_read_data_port.data <= ram_contents(get_ram_address(ram_read_data_port));
             end if;
             --------------------
-            if write_is_requested(self.ram_write_port) then
-                ram_contents(get_write_address(self.ram_write_port)) <= self.ram_write_port.write_buffer;
+            if write_is_requested(ram_write_port) then
+                ram_contents(get_write_address(ram_write_port)) <= ram_write_port.write_buffer;
             end if;
             --------------------
-            -- if write_is_requested(self.ram_write_port2) then
-            --     ram_contents(get_write_address(self.ram_write_port2)) <= self.ram_write_port2.write_buffer;
+            -- if write_is_requested(ram_write_port2) then
+            --     ram_contents(get_write_address(ram_write_port2)) <= ram_write_port2.write_buffer;
             -- end if;
             --------------------
+
+            create_processor_w_ram(
+                self                      ,
+                ram_read_instruction_port ,
+                ram_read_data_port        ,
+                ram_write_port            ,
+                ram_write_port2           ,
+                ram_array'length);
     ------------------------------------------------------------------------
             if example_filter_input.filter_is_requested then
                 request_low_pass_filter;
             end if;
-            self.registers(1) <= std_logic_vector(to_signed(example_filter_input.filter_input,self.registers(0)'length));
-
-            if decode(get_ram_data(self.ram_read_instruction_port)) = ready then
+            if decode(get_ram_data(ram_read_instruction_port)) = ready then
                result <= to_integer(signed(self.registers(0)));
             end if;
 
