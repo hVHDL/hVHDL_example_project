403e70183d000608ab4ac85e749af5cbe71f94ae
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-22 15:27:13 +0300

    update project to use multi_port ram for microprocessor

--- vhdl_sources.tcl
+++ vhdl_sources.tcl
@@ -2,7 +2,7 @@ add_vhdl_file_to_project $source_folder/../example_project_addresses_pkg.vhd
 
 add_vhdl_file_to_project $source_folder/efinix_top.vhd
 add_vhdl_file_to_project $source_folder/hVHDL_math_library/multiplier/configuration/multiply_with_2_input_and_output_registers_pkg.vhd
-add_vhdl_file_to_project $source_folder/hVHDL_math_library/multiplier/multiplier_base_types_18bit_pkg.vhd
+add_vhdl_file_to_project $source_folder/hVHDL_math_library/multiplier/multiplier_base_types_20bit_pkg.vhd
 add_vhdl_file_to_project $source_folder/hVHDL_math_library/multiplier/multiplier_pkg.vhd
 add_vhdl_file_to_project $source_folder/hVHDL_math_library/sincos/sincos_pkg.vhd
 add_vhdl_file_to_project $source_folder/hVHDL_math_library/first_order_filter/first_order_filter_pkg.vhd
@@ -35,3 +35,14 @@ add_vhdl_file_to_project $source_folder/hVHDL_floating_point/float_adder/float_a
 add_vhdl_file_to_project $source_folder/hVHDL_floating_point/float_multiplier/float_multiplier_pkg.vhd                             
 add_vhdl_file_to_project $source_folder/hVHDL_floating_point/float_alu/float_alu_pkg.vhd                                           
 add_vhdl_file_to_project $source_folder/hVHDL_floating_point/float_first_order_filter/float_first_order_filter_pkg.vhd             
+
+add_vhdl_file_to_project $source_folder/hVHDL_memory_library/fpga_internal_ram/ram_configuration_pkg.vhd           
+add_vhdl_file_to_project $source_folder/hVHDL_memory_library/multi_port_ram/multi_port_ram_pkg.vhd
+add_vhdl_file_to_project $source_folder/hVHDL_memory_library/multi_port_ram/ram_read_x2_write_x1.vhd
+add_vhdl_file_to_project $source_folder/hVHDL_memory_library/multi_port_ram/arch_rtl_read_x2_write_x1.vhd
+
+add_vhdl_file_to_project $source_folder/vhdl_microprogram_control_unit/testbenches/microcode_processor_pkg.vhd 
+add_vhdl_file_to_project $source_folder/vhdl_microprogram_control_unit/testbenches/test_programs_pkg.vhd       
+add_vhdl_file_to_project $source_folder/vhdl_microprogram_control_unit/testbenches/microinstruction_pkg.vhd    
+add_vhdl_file_to_project $source_folder/hVHDL_math_library/real_to_fixed/real_to_fixed_pkg.vhd                 
+add_vhdl_file_to_project $source_folder/hvhdl_example_interconnect/arch_microprogram_processor.vhd             
