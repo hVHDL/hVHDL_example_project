3f6acf7ea484ed24560c51b45313e45f038f002c
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-24 09:39:17 +0300

    add dual led blinker

--- radiant_build/certus_nx_top.vhd
+++ radiant_build/certus_nx_top.vhd
@@ -1,12 +1,12 @@
 library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
-    use ieee.math_real.all;
 	
 entity top is
     port (
         uart_rx : in std_logic ;
-        uart_tx : out std_logic
+        uart_tx : out std_logic;
+        led : out std_logic_vector(1 downto 0)
     );
 end entity top;
     
@@ -27,6 +27,9 @@ architecture rtl of top is
 
     signal clock_120mhz : std_logic := '0';
 	signal clk : std_logic;
+
+    signal led_counter : natural range 0 to 120e6 := 0;
+    signal led_state : std_logic := '0';
     
 --------------------------------------------------
 begin
@@ -37,6 +40,7 @@ begin
 	
     u_main_clocks : main_clock
     port map(clk, clock_120mhz);
+
 --------------------------------------------------
     u_hvhdl_example : entity work.efinix_top
     port map(
@@ -44,5 +48,23 @@ begin
         uart_rx     => uart_rx,
         uart_tx     => uart_tx);
 
+--------------------------------------------------
+    blink_leds : process(clock_120mhz)
+    begin
+        if rising_edge(clock_120mhz) then
+            if led_counter < 120e6 then
+                led_counter <= led_counter + 1;
+            else
+                led_counter <= 0;
+            end if;
+
+            if led_counter = 60e6 then
+                led_state <= not led_state;
+            end if;
+
+            led <= (others => led_state);
+
+        end if; --rising_edge
+    end process blink_leds;	
 --------------------------------------------------
 end rtl;
