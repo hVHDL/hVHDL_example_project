0d5f5919982d9617b4400c5d5eba43dbd6509156
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-24 08:17:49 +0300

    update build script

--- radiant_build/radiant_compile.tcl
+++ radiant_build/radiant_compile.tcl
@@ -2,21 +2,18 @@ variable tcl_path [ file dirname [ file normalize [ info script ] ] ]
 puts $tcl_path
 set outputDir ./output
 set source_folder $tcl_path/../source
-file mkdir $outputDir
+set project_name hvhdl_example
 
-set files [glob -nocomplain "$outputDir/*"]
-if {[llength $files] != 0} {
-    # clear folder contents
-    puts "deleting contents of $outputDir"
-    file delete -force {*}[glob -directory $outputDir *]; 
-}
+puts $source_folder
 
-
-prj_create -name hvhdl_example \
+prj_create -name $project_name \
     -dev LFD2NX-40-7BG196C \
     -impl "impl1" \
     -impl_dir $outputDir \
-    -synthesis "synplify" \
+    -synthesis "synplify" 
+
+puts $tcl_path
+puts $source_folder
 
 prj_set_strategy_value -strategy Strategy1 syn_vhdl2008=True
 prj_set_strategy_value -strategy Strategy1 {syn_pipelining_retiming=Pipelining and Retiming}
@@ -38,28 +35,4 @@ source $tcl_path/../vhdl_sources.tcl
 prj_set_impl_opt -impl "impl1" "top" "top"
 prj_add_source $tcl_path/cruvi_physical_constraints.pdc
 prj_run PAR -impl impl1
-# prj_strgy set_value -strategy Strategy1 syn_arrange_vhdl_files=True
-# prj_strgy set_value -strategy Strategy1 par_pathbased_place=On
-# prj_strgy set_value -strategy Strategy1 map_reg_retiming=True
-#
-#do not change this setting. If retiming is not on, Synplify will crash for some reason
-# prj_strgy set_value -strategy Strategy1 {syn_pipelining_retiming=Pipelining and Retiming}
-
-# prj_strgy set_value -strategy Strategy1 par_stop_zero=True
-
-
-# source $tcl_path/../vhdl_sources.tcl
-
-# prj_src add -exclude $tcl_path/example.lpf
-# prj_src enable $tcl_path/example.lpf
-# prj_src remove hvhdl_example.lpf
-# file delete -force hvhdl_example.lpf
-#
-# prj_run Synthesis -impl impl1
-# prj_run Translate -impl impl1
-# prj_run Map -impl impl1
-# prj_run PAR -impl impl1
-# prj_run Export -impl impl1 -task Bitgen
-# prj_run Export -impl impl1 -task Promgen
-# prj_project save
-prj_close
+prj_run Export -impl impl1
