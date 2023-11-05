82dc7dda8e9ba95f1113d2d5fe0881869b2ceea0
Jari Honkanen <jaarhonka@yahoo.com>
2023-11-05 11:38:15 +0200

    update radiant build script

--- radiant_build/radiant_compile.tcl
+++ radiant_build/radiant_compile.tcl
@@ -15,8 +15,7 @@ prj_create -name $project_name \
 puts $tcl_path
 puts $source_folder
 
-# setting this true will break the floating point filter for some reason
-# prj_set_strategy_value -strategy Strategy1 syn_vhdl2008=True
+prj_set_strategy_value -strategy Strategy1 syn_vhdl2008=True
 prj_set_strategy_value -strategy Strategy1 {syn_pipelining_retiming=Pipelining and Retiming}
 
 prj_add_source $tcl_path/IP/main_oscillator/main_oscillator.ipx
@@ -34,7 +33,7 @@ proc add_vhdl_file_to_library {vhdl_file library} {
 source $tcl_path/../vhdl_sources.tcl
 
 prj_add_source $tcl_path/pre_synth_constraints.sdc
-prj_set_impl_opt -impl "impl1" "top" "top"
 prj_add_source $tcl_path/cruvi_physical_constraints.pdc
+prj_set_impl_opt -impl "impl1" "top" "top"
 prj_run PAR -impl impl1
 prj_run Export -impl impl1
